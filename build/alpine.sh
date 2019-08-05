6977c5995fe536ba4f96e7cf36ac5e098c35978a
=lucasdonato <=lucaspolimig96@gmail.com>
2019-08-05 08:57:56 -0300

    alterando java

--- build/alpine.sh
+++ build/alpine.sh
@@ -6,6 +6,7 @@ apk update && \
     libxml2-dev \
     libxslt-dev \
     postgresql-dev \
-    curl -v -j -k -L -H http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux- i586.rpm> jdk-8u51-linux-i586.rpm
     curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
     && rm -rf /var/cache/apk/*
+
+    apk add openjdk8\
