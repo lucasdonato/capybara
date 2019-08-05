0a090561915cc11cbdaa0eb8b52fc4b8efc5634e
=lucasdonato <=lucaspolimig96@gmail.com>
2019-08-05 12:44:58 -0300

    sudo apt

--- build/alpine.sh
+++ build/alpine.sh
@@ -6,7 +6,7 @@ apk update && \
     libxml2-dev \
     libxslt-dev \
     postgresql-dev \
-    curl unzip openjdk8 * _bin.zip \
+    sudo apt-get instala o openjdk-8-jre \
     curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
     && rm -rf /var/cache/apk/*
 
