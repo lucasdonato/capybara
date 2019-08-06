46bd9a1c614d9abd2e85d7c034783842f81698cb
=lucasdonato <=lucaspolimig96@gmail.com>
2019-08-06 15:59:56 -0300

    alteracoes

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,19 +1,12 @@
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && \
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
 
-
-apt-add-repository ppa:qameta/allure
-apt-get update
-apt-get install allure
-
-
 apk update && \
 	apk add build-base \
     libxml2-dev \
     libxslt-dev \
     postgresql-dev \
-    curl unzip libexif udev chromium chromium-chromedriver nodejs wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
+    curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
     && rm -rf /var/cache/apk/*
-
 
     
